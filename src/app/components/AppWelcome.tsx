4bf7cb04b5d4c5214de2eb3fa6e3ae0190e4c787
beyourdelight <chutikarn.ruangp@gmail.com>
2025-05-27 11:36:30 +0700

    button

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,10 +1,18 @@
+'use client'
 export default function AppWelcome() {
     const title = 'Welcome to Cosci';
     const currentYear = <p>2025</p>;
     const isShow = true;
+
+    const handleClick = () => {
+        alert ('Hello, TypeScript!');
+    }
     return (
         <>
         <p>{title.toLocaleUpperCase()}</p>
+        {/* <button>className="bg-blue-700 p-3 m-3 text white") {
+        }</button> */}
+        ,<button className="bg-blue-700 p-3 m-3 text white rounded-lg"onClick={handleClick}>กดได้เลยจ้า</button>
         {currentYear}
         {
             isShow && <p>Date: 01/08/1994</p>
