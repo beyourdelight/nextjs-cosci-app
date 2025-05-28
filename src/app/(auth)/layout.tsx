82105bd1e14748fcea354284584b9e414f2d7a1c
beyourdelight <chutikarn.ruangp@gmail.com>
2025-05-28 11:23:18 +0700

    layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Kanit } from "next/font/google";
+import "../globals.css";
+
+
+const kanit = Kanit({
+    subsets:['thai'],
+        weight:['100', '200', '300', '400', '500', '600', '700', '800'],
+    display:'swap',
+});
+
+
+
+export const metadata: Metadata = {
+    title: "เข้าสู่ระบบ",
+        description: "ขายสินค้าของนักศึกษา ประจำปี2568",
+};
+
+export default function AuthLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+    <html lang="en">
+        <body
+        className={`${kanit.className} `}
+        >
+        
+        
+        {children}
+    </body>
+    </html>
+);
+}
