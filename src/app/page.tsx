d14dbea4925188a1bdfa6c175cdd36551ab06927
beyourdelight <chutikarn.ruangp@gmail.com>
2025-05-28 10:56:17 +0700

    HomeUpdate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero02 from "@/components/hero-02/hero-02";
-
-
+import Features01Page from "@/components/features-01/features-01";
+import Footer03Page from "@/components/footer-03/footer-03";
 export default function Home() {
   return (
     <div> 
       <Hero02 />
+      <Features01Page />
+      <Footer03Page />
     </div> //ใช้ <> and </>  แทน div ได้
   );
 }
