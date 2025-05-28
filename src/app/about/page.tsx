7964ac31d681b4ce693e446c70797ec9f9725cce
beyourdelight <chutikarn.ruangp@gmail.com>
2025-05-28 10:22:19 +0700

    about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,8 @@
+import Contact01Page from "@/components/contact-01/contact-01";
 export default function About() {
     return (
         <>
-        <p>About us</p>
+        <Contact01Page />
         </>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
+import Hero02 from "@/components/hero-02/hero-02";
 
-import AppWelcome from "./components/AppWelcome";
 
 export default function Home() {
   return (
     <div> 
-      <h1>Home Page</h1>
-      <hr />
-      <AppWelcome headTitle="Whats up man" isShow={true} />
-      <AppWelcome />
-      <AppWelcome />
-      <AppWelcome />
+      <Hero02 />
     </div> //ใช้ <> and </>  แทน div ได้
   );
 }
